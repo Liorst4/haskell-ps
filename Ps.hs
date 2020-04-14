f9f45ca7f668904d1aa4a40d0411838712081573
Lior Stern <liorst4@gmail.com>
2020-04-14 13:24:42 +0300

    Add user id

--- Ps.hs
+++ Ps.hs
@@ -1,24 +1,26 @@
 import System.Posix.Types
+import System.Posix.Files
 import System.IO
 import System.Directory
 import Control.Monad
 
-procPath :: FilePath
-procPath = "/proc"
 
--- TODO: Don't change current directory
-listDirectoryDirectories :: FilePath -> IO [FilePath]
-listDirectoryDirectories path = withCurrentDirectory path $ do
-  items <- listDirectory "."
-  filterM doesDirectoryExist items
+data ProcessEntry = ProcessEntry { id :: ProcessID
+                                 , user :: UserID }
+  deriving (Show)
 
--- TODO: Find a replacement for these functions
-isDigit :: Char -> Bool
-isDigit c = c `elem` "0123456789"
-isInt :: String -> Bool
-isInt = all isDigit
 
-listProcesses :: IO [ProcessID]
+listProcesses :: IO [ProcessEntry]
 listProcesses = do
-  procDirs <- listDirectoryDirectories procPath
-  return (map read (filter isInt procDirs) :: [ProcessID])
+  procEntries <- listDirectory procPath
+  let procEntriesFullPath = map (procPath ++) procEntries
+  procEntriesStat <- mapM getFileStatus procEntriesFullPath -- TODO: Race condition?
+  let procEntriesWithStat = zip procEntries procEntriesStat
+  let processDirectoriesWithStat = filter isProcessDirectory procEntriesWithStat
+  return [ProcessEntry (read (fst x) :: ProcessID) (fileOwner (snd x)) | x <- processDirectoriesWithStat]
+  where
+    procPath = "/proc/"
+    isProcessDirectory (name, stat) = isDirectory stat && isInt name
+    -- TODO: Find a replacement for these functions
+    isInt = all isDigit
+    isDigit c = c `elem` "0123456789"
